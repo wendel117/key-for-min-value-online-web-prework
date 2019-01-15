7c9c139493dab4a83e24a82e14fc8c51fb085ba8
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-15 19:59:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,4 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-require pry
+
 def key_for_min_value(name_hash)
-  min = name_hash.values.sort
-  name_hash.key(min.first)
-end
